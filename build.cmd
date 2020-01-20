8f05927f5a18660672aa95ea2f001e47ace7a1bc
Oliver Schmidt <ol.sc@web.de>
2020-01-20 12:56:50 +0100

    Deleted cmd file made obsolete by Makefile.

--- build.cmd
+++
@@ -1,4 +0,0 @@
-copy apple2\template.dsk Penetrator.dsk
-java -jar apple2\AppleCommander-win64-1.5.0.jar -p  Penetrator.dsk penetrat.system sys < penetrator-apple2.apple2.loader
-java -jar apple2\AppleCommander-win64-1.5.0.jar -as Penetrator.dsk penetrat        bin < penetrator-apple2.apple2
-apple2\Applewin.exe -d1 penetrator.dsk
